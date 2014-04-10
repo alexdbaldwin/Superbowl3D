906db93a4c0d30e04741c7690ec36584957b85a9
Jesper Sahlin <jesper.sahlin@gmail.com>
2014-04-10 15:30:05 +0200

    Första fysikcommiten
    Simon och Jesper har lekt runt lite! Inget av större intresse för någon
    atm.

---
+++ Assets/Scripts/BallControlScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControlScript : MonoBehaviour {
+	float turnSpeed = 10.2f;
+	float trust = 30.0f;
+	float maxTurnSpeed = 2.5f; 
+	public float velX = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+				float horizontal = Input.GetAxis ("Horizontal");
+				float vertical = Input.GetAxis ("Vertical");
+				if (rigidbody.velocity.x < maxTurnSpeed && rigidbody.velocity.x > -maxTurnSpeed) {
+						rigidbody.AddForce (new Vector3 (-horizontal * turnSpeed, 0, -vertical * trust));
+				}
+//			Test
+
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0, 0, 100, 100), rigidbody.velocity.x.ToString());
+		GUI.Label (new Rect (0, 20, 100, 100), "Fan va jag e störig");
+	}
+}
---
+++ Assets/Scripts/CameraPositioningScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPositioningScript : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y + 1, target.transform.position.z + 3);
+		transform.LookAt (target.transform);
+	}
+}
