f644245bbca67873db788650713c961842699ad6
Alex Baldwin <alexander.d.baldwin@gmail.com>
2014-05-09 01:03:50 +0200

    Finished turret + jump pad

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	
+	float speed = 15.0f;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.name != "TurretBarrel") {
+			Destroy (gameObject);
+		}
+	}
+}
